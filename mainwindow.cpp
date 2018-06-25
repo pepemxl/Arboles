35e4423312f74820b89b8e38e22f6bb697d5f0af
José Luis Alonzo <joseluisalonzo@golstats.com>
2018-06-25 09:59:06 -0500

    Agregando datos de control

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->S1.test_01();
 }
 
 MainWindow::~MainWindow()
--- searchtree.h
+++ searchtree.h
@@ -5,17 +5,34 @@
 #include <vector>
 #include <algorithm>
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 #include <limits.h>
 
 #define REP(i,n) for(int i = 0,n_=(n);i < n_;++i)
 #define FOR(i,a,b) for(int i = a,n_=(b)+1;i < n_;++i)
 #define FORD(i,b,a) for(int i = b,n_=(a);i > n_;++i)
 
+/**
+ * \brief Esta estructura guardar&aacute; todos los datos que necesitamos
+ * para crear nuestro arbol.
+ * \todo Para poder calcular los pesos sera necesario tener una
+ * estructura lombriz que guarde todas las coordenadas que componen
+ * a la lombriz.
+ */
 struct AdjListNode{
     int dest;
     int weight;
+    std::vector<float> vectorScores;
+    class Lombris *ptrLombris;
+    // La siguiente informacion se encontrara dentro de la estructura Lombris
+    //int frameInitial;//!< Guardamos el frame donde inicia
+    //int frameFinal;//!< Guardamos el frame donde termina
+    //int keypointId;//!< Guardamos el identificador de la lombriz
+    //float xField;//!< x_field del frame inicial
+    //float yField;//!< y_field del frame inicial
+    //float xOriginal;//!< x_original del frame inicial
+    //float yOriginal;//!< x_original del frame inicial
     struct AdjListNode* next;
 };
 
@@ -25,6 +42,7 @@ struct AdjList{
 
 struct Graph{
     int V;
+    //std::vector<struct AdjList> vectorAdj;
     struct AdjList *array;
 };
 
@@ -43,9 +61,28 @@ struct MinHeap{
 
 class Lombris{
 public:
-    Lombris(){}
-    ~Lombris(){}
+    Lombris();
+    Lombris(unsigned long int frameInitial_,float xField_,float yField_,float xOriginal_,float yOriginal_);
+    ~Lombris();
+    int numberOfVertexes;
+    bool addPositionsOnFrame(float xField_,float yField_,float xOriginal_,float yOriginal_);
+    unsigned long int frameInitial;//!< Guardamos el frame donde inicia
+    unsigned long int frameFinal;//!< Guardamos el frame donde termina
+    unsigned long int keypointId;//!< Guardamos el identificador de la lombriz
+    std::vector<float> xField;//!< x_field del frame i
+    std::vector<float> yField;//!< y_field del frame i
+    std::vector<float> xOriginal;//!< x_original del frame i
+    std::vector<float> yOriginal;//!< y_original del frame i
+    int getNumberOfVertexes() const;
+    void setNumberOfVertexes(int value);
+    unsigned long getFrameInitial() const;
+    void setFrameInitial(unsigned long value);
+    unsigned long getFrameFinal() const;
+    void setFrameFinal(unsigned long value);
+    unsigned long getKeypointId() const;
+    void setKeypointId(unsigned long value);
 };
+
 
 class Node{
 public:
@@ -66,9 +103,9 @@ public:
     searchtree();
     ~searchtree();
     searchtree(Node*);
-
-    AdjListNode *newAdjListNode(int dest, int weight);
+    unsigned long int memoryRamBytes;
     Graph *createGraph(int V);
+    AdjListNode *newAdjListNode(int dest, int weight);
     void addEdge(Graph *graph, int src, int dest, int weight);
     MinHeapNode *newMinHeapNode(int v, int dist);
     MinHeap *createMinHeap(int capacity);
@@ -81,6 +118,19 @@ public:
     void printArr(int dist[], int n);
     void test_01();
     void dijkstra(Graph *graph, int src);
+    /// Getter Setter
+    unsigned long getMemoryRamBytes() const;
+    void setMemoryRamBytes(unsigned long value);
+    void addMemoryRamBytes(unsigned long value);
 };
+
+/*class CGraphDirected{
+    int V; //!< Numero de vertices actual
+    //list< pair<int, int> > *adj;
+public:
+    Graph(int V);
+    void addEdge(int u, int v, int w);
+    void shortestPath(int s);
+};*/
 
 #endif // SEARCHTREE_H
